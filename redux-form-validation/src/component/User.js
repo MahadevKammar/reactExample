84b7ec4632c68d98d5a7532723aebdc06298b478
Mahadev <mahadevkammar408@gmail.com>
2018-06-21 19:26:22 +0530

    commit 2

--- redux-form-validation/src/component/User.js
+++ redux-form-validation/src/component/User.js
@@ -9,11 +9,11 @@ import {authenticating} from '../store/action/action'
 
 let loading=false;
 //rednering the input
-let renderInput = ({ input, meta, label }) => {
+let renderInput = ({ input, meta, label,type }) => {
   return <div >
     {/* <pre> {JSON.stringify(meta,0,2)}</pre> */}
     <label>{label}</label>
-    <input {...input} />
+    <input {...input} type={type} />
     {meta.error && meta.touched && <span>{meta.error}</span>}
   </div>
 }
@@ -21,20 +21,17 @@ let renderInput = ({ input, meta, label }) => {
 
 
 let User = (props) => { 
-
-  
-  //injected by Redux-form
-   const { handleSubmit,submitting } =  props
- 
-var showValues1 = (values) => {
-  props.authenticatingD(true);
-  console.log("Is Authenticated? " +props.authenticating)
-  console.log(loading)
-  setTimeout(function(){      
-    props.authenticatingD(false)
-     window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);     
-     console.log(loading);
-   
+ //injected by Redux-form
+    const { handleSubmit,submitting } =  props 
+    var showValues1 = (values) => {
+    props.authenticatingD(true);
+    console.log("Is Authenticated? " +props.authenticating)
+    console.log(loading)
+    setTimeout(function(){      
+      props.authenticatingD(false)
+      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);     
+      console.log(loading);
+    
     },2000);
  }
   
@@ -48,19 +45,19 @@ var showValues1 = (values) => {
            loading={true} 
                       />
            <p className="layout1">submitting...</p>
-       </div>):(
+         </div>):(
           <form onSubmit={handleSubmit(showValues1)} className="signup-form">
             <h1 className="signup-title">Enter User Details</h1>
             <Field name="userName" label="User Name:" type="text" component={renderInput} />
             <Field name="email" label="Email:" type="text" component={renderInput} />
-            <Field name="password" label="Password:" type="text" component={renderInput} />
+            <Field name="password" label="Password:" type="password" component={renderInput} />
             <Field name="mobile" label="Mobile:" type="text" component={renderInput} />
             <Field name="zip" label="Zip:" type="text" component={renderInput} />
             <div align="center">
               <button type="submit" className="submit-btn" disabled={submitting} >Submit</button>
             </div>      
           </form>
-      )
+         )
        }
 </div>
   )
